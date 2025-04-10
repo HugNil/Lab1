27eeeee3d6dc1afb6c08f63c0837151ba555df75
HugNil <02hugo.nilsson@gmail.com>
2025-04-10 12:49:49 +0200

    Added a api for dish by id

--- src/routes/dishRoutes.js
+++ src/routes/dishRoutes.js
@@ -14,3 +14,22 @@ router.get('/api/dishes', async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 });
+
+
+
+
+// GET - Get dish by ID
+router.get('/api/dish', async (req, res) => {
+    const { name } = req.query;
+  
+    try {
+        if (!name) {
+            return res.status(400).json({ message: 'Name parameter is required' });
+        }
+  
+        const dishes = await Dish.find({ name: { $regex: name, $options: 'i' } });
+        res.json(dishes);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
